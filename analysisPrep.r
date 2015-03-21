6ff495eeaf49b98f61180dd5ca55c6b16a1e1ccc
kgilds <kevingilds@gmail.com>
2015-03-20 22:06:51 -0400

    add analysis script

---
+++ analysisPrep.r
@@ -0,0 +1,36 @@
+
+####Reading Grades
+saveRDS(mq2Reading2, file="Q2.rds")
+
+saveRDS(mq1Reading2, file="Q1.rds")
+
+
+#####Lang Arts
+saveRDS(mq2Lang, file="Q2lang.rds")
+
+saveRDS(mq1Lang,file="Q1lang.rds")
+
+####Unexcused Abs
+saveRDS(q2Uabs, file="Q2Uabs.rds")
+
+saveRDS(q1Uabs, file="Q1Uabs.rds")
+
+####Excused Abs
+saveRDS(q2Eabs, file="Q2excused.rds")
+
+saveRDS(q1Exabs, file="Q1excused.rds")
+
+###In School Suspensions
+saveRDS(q2Is, file="q2Inschool.rds")
+
+saveRDS(q1Is, file="q1Inschool.rds")
+
+####Out of School Suspensions
+saveRDS(q2Os, file="q2Outschool.rds")
+saveRDS(q1Os, file="q1Outschoo.rds")
+
+####Behavorial Referrals
+
+saveRDs(q2Bev, file="q2bevRef.rds")
+
+saveRDS(q1Bev, file="q1bevRef.rds")
