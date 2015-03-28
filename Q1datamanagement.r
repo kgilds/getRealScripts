80f81a87a8eaebe0d931107ff576dee915bb950f
kgilds <kevingilds@gmail.com>
2015-03-28 15:20:01 -0400

    updated q1dm and analysisPrep

--- Q1datamanagement.r
+++ Q1datamanagement.r
@@ -1,4 +1,4 @@
-setwd("C:/Users/kevin/Dropbox/GetReal/Data/March 2015")
+setwd("C:/Users/kevin/Dropbox/GetReal/Data/April 2015")
 
 #### Load the Libraries
 library(dplyr)
@@ -6,10 +6,9 @@ library(reshape2)
 
 
 ###Read the data frame
-grades <- read.csv("gradesQ1.03.13.csv", skip=1)
+grades <- read.csv("gradesQ1.03.27.csv", skip=1)
 
-#### Reade the Name of the columns
-names(grades)
+
 
 ### Demo Variables
 colnames (grades) [7] <- "council"
@@ -162,35 +161,6 @@ colnames (mq1Reading2) [3] <- "readingCourses"
 
 
 
-######Read Grade Summary
-q1Reading <-with(mq1Reading2, table(girlCode,readingGrade))
-
-q1Reading
-
-dim(q1Reading)
-
-saveRDS(q1Reading, file="q1Reading.rds")
-
-
-
-q1Reading <-as.data.frame(q1Reading)
-
-
-names(q1Reading)
-
-q1Reading <- filter(q1Reading, Freq >=1)
-
-
-summary(q1Reading)
-
-tail(q1Reading)
-
-
-
-
-
-####Summary by Council#################
-with(mq1Reading2,table(council, value))
 
 
 
@@ -217,55 +187,13 @@ levels(mq1Lang$value) <- c("F", "D", "C", "B", "A")
 colnames (mq1Lang) [4] <- "langArtsGrade"
 colnames (mq1Lang) [3] <- "langArtsCourses"
 
-######## This analysis is analyis script and needs to be moved. 
-
-names(mq1Lang)
-
-q1langArtsgrade <-with(mq1Lang, table(girlCode, langArtsGrade))
-
-q1langArtsgrade <- as.data.frame(q1langArtsgrade)
-
-q1langArtsgrade <-filter(q1langArtsgrade, Freq >=1)
-
-saveRDS(q1langArtsgrade, file="q1langArts.rds")
-
-head(q1langArtsgrade)
-
-summary(q1langArtsgrade)
-
-SaveRDS()
-
-
-q1$freeLang2
-
-
-#####Grade summary by council
-with(mq1Lang, table(council, value))
 
 ########### Unexcused Absences
 
 q1Uabs <- gradesUnique[, c(7,8, 11)]
 
-head(q1Uabs)
-
-summary(q1Uabs)
-
-str(q1Uabs)
 
 q1Uabs$unexusedAbs <- as.numeric(q1Uabs$unexusedAbs)
-
-with(q1Uabs, tapply(unexusedAbs, council, summary))
-
-with(q1Uabs,table(council, unexusedAbs))
-
-
-
-with(q1Uabs, table(council,unexusedAbs >= 5))
-with(q1Uabs, table(council,unexusedAbs >=10))
-with(q1Uabs, table(council,unexusedAbs >=15))
-with(q1Uabs, table(council,unexusedAbs >=20))
-with(q1Uabs, table(council,unexusedAbs >=25))
-with(q1Uabs, table(council,unexusedAbs >=30))
 
 
 
@@ -274,36 +202,18 @@ with(q1Uabs, table(council,unexusedAbs >=30))
 
 q1Exabs <- gradesUnique[, c(7,8,12)]
 
-head(q1Exabs)
-
-summary(q1Exabs)
 
 q1Exabs$excuseddAbs <- as.numeric(q1Exabs$excusedAbs) 
 
-with(q1Exabs, table(excusedAbs))
 
-with(q1Exabs, table(council, excusedAbs <=0))
-with(q1Exabs, table(council, excusedAbs >=5))
-with(q1Exabs, table(council, excusedAbs >=10))
-with(q1Exabs, table(council, excusedAbs >=15))
-with(q1Exabs, table(council, excusedAbs >=20))
-with(q1Exabs, table(council, excusedAbs >=30))
-
-
-
-with(q1Exabs, tapply(excusedAbs, council,summary))
 
 #######Behavioral Referrals
 
 q1Bev <- gradesUnique[, c(7,8,13)]
 
 
-
 q1Bev$behaviorRef <-as.numeric(q1Bev$behaviorRef)
 
-with(q1Bev, table(behaviorRef, council))
-
-with(q1Bev, tapply(behaviorRef,council, summary))
 
 
 ####################In School Suspensions##############
@@ -314,9 +224,6 @@ q1Is <- gradesUnique[, c(7,8,14)]
 q1Is$inSchoolSusp <- as.numeric(q1Is$inSchoolSusp)
 
 
-with(q1Is, table(inSchoolSusp, council))
-
-with(q1Is, tapply(inSchoolSusp, council, summary))
 
 ##################Out of School Suspensions#############3
 
@@ -324,11 +231,6 @@ q1Os <- gradesUnique[, c(7,8,15)]
 
 q1Os$outofSchoolSusp <- as.numeric(q1Os$outofSchoolSusp)
 
-with(q1Os, table(outofSchoolSusp))
-
-with(q1Os, tapply(outofSchoolSusp, council, summary))
-
-with(q1Os, table(outofSchoolSusp, council))
 
 
 #################Expelled###########################
@@ -336,30 +238,4 @@ with(q1Os, table(outofSchoolSusp, council))
 q1Exp <-gradesUnique[, c(7,8,16)]
 
 
-with(q1Exp, table(expelled))
-
-with(q1Exp, table(council, expelled))
-
-
-############Free Lang###############3
-
-q1freeLan <-gradesUnique[, c(7,8, 46,47 )]
-
-q1freeLan
-
-grep("eng", q1freeLan$freeLang2,ignore.case=TRUE)
-
-
-gulfcoast <-filter(q1, council=="Girl Scouts of Gulfcoast Florida")
-
-gulfcoast
-
-outcomeGrades(merge())
-
-############################Behavior Data###########################3
-
-
-q1behavior <- gradesUnique [, c(8,7,11,12,13,14,15)]
-
-saveRDS(q1behavior, file="q1behavior.rds")
 
--- analysisPrep.r
+++ analysisPrep.r
@@ -1,7 +1,7 @@
 library(sqldf)
 library(reshape2)
 library(dplyr)
-getRealdb<- dbConnect(SQLite(), dbname="tempGetReal.sqlite")
+getRealdb<- dbConnect(SQLite(), dbname="getRealApril.sqlite")
 
 
 ####Reading Grades
@@ -13,7 +13,7 @@ Q2Reading$value<-as.numeric(Q2Reading$value)
 
 dbWriteTable(conn = getRealdb, name = "Q2Reading", value= Q2Reading, row.names=FALSE, overwrite=TRUE)
 
-
+####Q1 Reading#############
 saveRDS(mq1Reading2, file="Q1.rds")
 Q1Reading <-readRDS("Q1.rds")
 Q1Reading <-na.omit(Q1Reading)
@@ -32,6 +32,7 @@ Q2Lang$value <-as.numeric(Q2Lang$value)
 dbWriteTable(conn = getRealdb, name = "Q2Lang", value= Q2Lang, row.names=FALSE, overwrite=TRUE)
 
 
+#####Q1 Lang Arts#############
 saveRDS(mq1Lang,file="Q1lang.rds")
 Q1Lang <-readRDS("Q1lang.rds")
 Q1Lang <-na.omit(Q1Lang)
@@ -42,21 +43,32 @@ dbWriteTable(conn = getRealdb, name = "Q1Lang", value= Q1Lang, row.names=FALSE, 
 
 
 ####Unexcused Abs
+###Q2
 saveRDS(q2Uabs, file="Q2Uabs.rds")
+bWriteTable(conn = getRealdb, name = "Q2Uabs", value= Q2Uabs, row.names=FALSE, overwrite=TRUE)
 
+###Q1
 saveRDS(q1Uabs, file="Q1Uabs.rds")
+Q1Uabs<-readRDS("Q1Uabs.rds")
+dbWriteTable(conn= getRealdb, name = "Q1Uabes", value = Q1Uabs,row.names=FALSE, overwrite=TRUE)
+
 
 ####Excused Abs
 saveRDS(q2Eabs, file="Q2excused.rds")
+dbWriteTable(conn = getRealdb, name = "Q2Eabs", value= Q2Eabs, row.names=FALSE, overwrite=TRUE)
+
 
 saveRDS(q1Exabs, file="Q1excused.rds")
+Q1Eabs<-readRDS("Q1excused.rds")
+dbWriteTable(conn = getRealdb, name = "Q1Eabs", value= Q1Eabs, row.names=FALSE, overwrite=TRUE)
+
 
 ###In School Suspensions
 saveRDS(q2Is, file="q2Inschool.rds")
 q2InSchool <-readRDS("q2Inschool.rds")
 dbWriteTable(conn = getRealdb, name = "q2InSchool", value= q2InSchool, row.names=FALSE, overwrite=TRUE)
 
-
+#####Q1 In School Suspenions#################3
 saveRDS(q1Is, file="q1Inschool.rds")
 q1InSchool <-readRDS("q1Inschool.rds")
 dbWriteTable(conn = getRealdb, name = "q1InSchool", value= q1InSchool, row.names=FALSE, overwrite=TRUE)
@@ -67,7 +79,7 @@ saveRDS(q2Os, file="q2Outschool.rds")
 q2OutSchool <-readRDS("q2Outschool.rds")
 dbWriteTable(conn = getRealdb, name = "q2OutSchool", value= q2OutSchool, row.names=FALSE, overwrite=TRUE)
 
-
+#######Q1 Out of School Suspensions################
 saveRDS(q1Os, file="q1Outschool.rds")
 q1OutSchool <-readRDS("q1Outschool.rds")
 dbWriteTable(conn = getRealdb, name = "q1OutSchool", value= q1OutSchool, row.names=FALSE, overwrite=TRUE)
@@ -79,7 +91,7 @@ saveRDS(q2Bev, file="q2bevRef.rds")
 q2Bev <-readRDS("q2bevRef.rds")
 dbWriteTable(conn = getRealdb, name = "q2Bev", value= q2Bev, row.names=FALSE, overwrite=TRUE)
 
-
+####Q1 Behavioral Referrals###########
 saveRDS(q1Bev, file="q1bevRef.rds")
 q1Bev <-readRDS("q1bevRef.rds")
 dbWriteTable(conn = getRealdb, name = "q1Bev", value= q1Bev, row.names=FALSE, overwrite=TRUE)
