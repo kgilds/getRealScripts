65e32262f84a00b390e2717060909a20b601168d
kgilds <kevingilds@gmail.com>
2015-05-17 15:39:40 -0400

    added dotchart script.
    still somewhat exploratory script; needs to be cleaned up

--- gradeOutcomeTable.r
+++ gradeOutcomeTable.r
@@ -24,10 +24,13 @@ dbWriteTable(conn = getRealdb, name = "lastYear1", value= lastYear1, row.names=F
 ###Read the data from database#######3
 reading_A <-dbReadTable(getRealdb, "readingFinal")
 
-reading_B <-select(reading_A, council, Q1grade, Q2grade)
+reading_B <-select(reading_A, council, Q1grade, Q2grade, diff)
 
 reading_A$council <-as.factor(reading_A$council)
 
+plot(reading_B$Q1grade, reading_B$Q2grade)
+
+reading_B_tbl <-with(reading_B,table(Q1grade, Q2grade))
 
 
 str(reading_A)
@@ -74,6 +77,8 @@ belowC_df$council <-as.factor(belowC_df$council)
 
 belowC_tbl <-with(belowC_df, table(council))
 
+plot(belowC_tbl)
+
 
 belowC <- with(reading_A, table(Q1grade < 3 & Q2grade <3))
 
@@ -83,10 +88,14 @@ belowC <- with(reading_A, table(Q1grade < 3 & Q2grade <3))
 
 failToPass_df <-filter(reading_A, Q1grade <3 & Q2grade >=3)
 
-failToPass_council <- with(failToPass_df,table(council))
+failToPass_council <- with(failToPass_df,table(council,diff))
+
+dotchart(failToPass_council)
 
 
 failPass_tbl <-with(reading_A, table( Q1grade <3 & Q2grade >=3))
+
+plot(failPass_tbl)
 
 
 ###############FailToFail#################################
@@ -100,7 +109,18 @@ CDrop <- with(reading_A,table(Q1grade >=3 & Q2grade <3))
 
 ####################C and higher#################3
 
+CPlus.df <-filter(reading_B, Q1grade >=3 & Q2grade >=3)
+
+CPlus.df.tbl <- with(CPlus.df,table(council, diff))
+
+####Graph
+
+dotchart(CPlus.df.tbl, col="blue")
+
+
 CPlus <-with(reading_A, table(Q1grade >=3 & Q2grade >=3))
+
+
 
 
 #######################Lang Arts Final#######################33
