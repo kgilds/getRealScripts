8d235d465222b1f278a9d61bd520fddc75fb73f4
kgilds <kevingilds@gmail.com>
2015-06-14 21:22:19 -0400

    added ae outcome table scripts.
    added ae outcome table scripts; started working on script for T-tests
    but not working.

--- matchedgirlSurveyAnalysis.r
+++ matchedgirlSurveyAnalysis.r
@@ -1,3 +1,7 @@
+library(dplyr)
+library(reshape2)
+
+
 
 #### Read the files from girlSurvey Data Management, this will need to be updated. 
 girlPre <- readRDS("girlPre067.rds")
@@ -12,7 +16,7 @@ prePost <- left_join(girlPost, girlPre, by = "girlCode", Copy=FALSE)
 
 
 ####################Pair down date frame into the Outcomes that we are interested in#########
-hr <- prePost1 <-select(prePost, Time.x, council.x, girlCode, hr.avg.x, hr.avg.y)
+hr <- select(prePost, Time.x, council.x, girlCode, hr.avg.x, hr.avg.y)
 
 
 #########################Match the completed cases################################
@@ -27,6 +31,17 @@ hr1<-cbind(hr1, diffhr)
 
 
 ######################Basic Analsis############################################
+
+hr2 <- select(hr1, Time.x, girlCode,hr.avg.x, hr.avg.y)
+
+hr3_melt <-melt(hr2, id.var=c("girlCode", "Time.x") ,measure.vars=c("hr.avg.x", "hr.avg.y"))
+
+
+hr3_melt <-levels(hr2_melt$Time.x)
+
+t.test(value~Time.x, data=hr2_melt, paired=TRUE)
+
+
 
 
 ##### General Outcome Tables###################################################
@@ -68,13 +83,52 @@ plot(post_hr_outcome$diffhr)
 abline(h=0)
 
 
+############################Academic Engagement##########################333
+ae <- select(prePost, Time.x, council.x, girlCode,ae.avg.x, ae.avg.y )
 
 
 
+ae1<-ae %>% filter(complete.cases(ae)) 
 
 
 
+#############Find the difference
 
+ae_diff <- (ae1$ae.avg.x- ae1$ae.avg.y)
+
+############Bind ae_diff to ae1
+ae2 <- cbind (ae1, ae_diff)
+
+
+###################General Outcome Table####################3
+
+ae_table_pre <-with(ae2,table(ae.avg.y >=4.5))
+
+ae_table_post <-with(ae2,table(ae.avg.x >=4.5))
+
+
+###########################################################
+
+
+##########From not achieving to achieving####################
+
+ae_not_a <- filter(ae2, ae.avg.y < 4.5)
+
+ae_not_a_tbl <-with(ae_not_a,table(ae.avg.x >=4.5))
+
+ae_not_a_tbl
+
+
+
+#####Did not Achieve hr post but increased score####
+
+ae_not_achieved <-filter(ae2, ae.avg.y < 4.5)
+
+ae_not_achieve_tbl <- with(ae_not_achieved,table(ae_diff >0))
+
+ae_not_achieve_tbl
+
+mean(ae_not_achieved$ae_diff)
 
 
 
