61a924be44545ff2b5f1633af08441faf0fcc405
kgilds <kevingilds@gmail.com>
2015-07-04 21:57:58 -0400

    updated data set and workaround girlCode issues

--- matchedgirlSurveyAnalysis.r
+++ matchedgirlSurveyAnalysis.r
@@ -4,13 +4,113 @@ library(reshape2)
 
 
 #### Read the files from girlSurvey Data Management, this will need to be updated. 
-girlPre <- readRDS("girlPre067.rds")
+girlPre <- readRDS("girlPre07042015.rds")
 
-girlPost <-readRDS("girlPost067.rds")
+girlPost <-readRDS("girlPost07042015.rds") #N=735
 
-#####Join df by girlCod using Dplyr library
-prePost <- left_join(girlPost, girlPre, by = "girlCode", Copy=FALSE)
 
+
+######### Fix Girl Code###############################################
+
+######################Gateway#######################################
+gateway <-filter(girlPost, council == "Gateway Council")
+
+
+###Fix
+girlCode2<- sub("313", "", gateway$girlCode)
+
+fixgirlCode<- sub("", "313", girlCode2)
+
+gateway$girlCode <- fixgirlCode
+
+
+#####Test Girl Codes####################
+gateway$girlCode
+
+head(gateway$girlCode)
+
+tail(gateway$girlCode)
+
+head(gateway$girlCode, n=250)
+
+############New Post Data frames################
+
+dim(gateway)
+
+str(gateway)
+
+
+gatewayPost <- filter(gateway, Time == "Post")
+
+
+GatewayprePost <- left_join(girlPost, girlPre, by = "girlCode", Copy=FALSE)
+
+
+#########################Citrus############################################
+
+citrus <- filter(girlPost, council == "Citrus Council")
+
+girlCodeCitrus<- sub("312", "", citrus$girlCode)
+
+fixgirlCodeCitrus<- sub("", "312", girlCodeCitrus)
+
+citrus$girlCode <- fixgirlCodeCitrus
+
+
+head(citrus$girlCode, n=100)
+
+
+citrusPre <-filter(citrus, Time == "Pre")
+
+citrusPost <-filter(citrus, Time == "Post")
+
+dim(citrusPost)
+
+citrusPre
+
+
+
+#######################Panhandle
+
+panhandle <-filter(girlPost, council == "Panhandle Council")
+
+
+###Fix
+girlCodePanhandle<- sub("322", "", panhandle$girlCode)
+
+fixgirlCodePanhandle<- sub("", "322", girlCodePanhandle)
+
+panhandle$girlCode <- fixgirlCodePanhandle
+
+
+head(panhandle$girlCode, n=122)
+
+
+
+
+
+######################Unmatched Girl Codes##############################
+
+prePost_anti <-anti_join(girlPost,girlPre, by ="girlCode", Copy=FALSE)
+
+
+######################### Exclude and add Gateway and Citrus back and Panhandle ############################
+
+excludeGatewayCitrusPanhandle <-filter(girlPost, council != "Citrus Council" & council != "Gateway Council" & council != "Panhandle Council") #n=292
+
+addGatewayGirlPost <- rbind(gateway, excludeGatewayCitrusPanhandle) #N=564
+
+addedCitrusPost <-rbind(citrus, addGatewayGirlPost) #N=611
+
+girlPost1 <- rbind(panhandle, addedCitrusPost) #N=735
+
+
+
+#####Join df by girlCod using Dplyr library###################################################
+prePost <- left_join(girlPost1, girlPre, by = "girlCode", Copy=FALSE)
+
+
+prePost_anti <-anti_join(girlPost,girlPre, by ="girlCode", Copy=FALSE)
 
 
 
@@ -181,7 +281,7 @@ am.not.achieve.tbl <- with(not_achieved,table(diffhr >0))
 mean(am.not.achieved$diffam)
 
 
-####Achieved Hr Post Outcome#####################
+####Achieved Academic Post Outcome#####################
 
 post.am.outcome <- filter(am1, am.avg.x >=4.8)
 
